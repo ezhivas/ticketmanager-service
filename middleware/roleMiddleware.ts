cef78243be1c0f47c2e44841c3587f5e0d57863b
Serhii Vasylets <akvatonk@gmail.com>
2025-11-27 20:57:39 +0200

    fix: get rid of any warnings

--- middleware/roleMiddleware.ts
+++ middleware/roleMiddleware.ts
@@ -4,14 +4,19 @@ const roleMiddleware = () => {
     return (req: Request, res: Response, next: NextFunction) => {
         try{
             // const user = (req as any).user;
-            const userRole = (req as any).user?.role;
+            const userRole = req.user?.role;
             if(!userRole || userRole !== 'admin') {
                 res.status(401).send('Access denied');
                 return;
             }
             next();
-        } catch (error: any){
-            res.status(500).json({error: error.message});
+        } catch (error) {
+            if (error instanceof Error) {
+                res.status(400).json({ error: error.message });
+            } else {
+                // if unknown type
+                res.status(500).json({ error: 'Unknown error occurred' });
+            }
         }
     };
 };
